c067c16bf151622f5490beab82dcdcdbd97e14f9
AceOne4 <ahmedm.yassin14@gmail.com>
2024-04-05 16:10:35 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <section></section>
+      <section></section>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <img src="../../icons_assets/Logo.svg" alt="logo" />
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservation</a>
+        </li>
+        <li>
+          <a href="/">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
